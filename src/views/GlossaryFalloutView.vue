97f82745f7717dc99cde6e79e3c070c9907ad583
Alex Kiselyov <akiselyov@ukr.net>
2025-10-19 10:57:53 +0400

    Glossary: enable all Fallout games

--- src/views/GlossaryFalloutView.vue
+++ src/views/GlossaryFalloutView.vue
@@ -13,9 +13,9 @@ const falloutConfig: GlossaryConfig = {
 	localStorageKey: 'glossary-fallout-games',
 	defaultGames: ['fallout 4'],
 	gameCheckboxes: [
-		{ id: 'Fallout 1', name: 'Fallout (1997)', icon: '/img/icons/fallout1.png', disabled: true },
-		{ id: 'Fallout 2', name: 'Fallout 2 (1998)', icon: '/img/icons/fallout2.png', disabled: true },
-		{ id: 'Tactics', name: 'Fallout Tactics (2001)', icon: '/img/icons/tactics.png', disabled: true },
+		{ id: 'Fallout 1', name: 'Fallout (1997)', icon: '/img/icons/fallout1.png' },
+		{ id: 'Fallout 2', name: 'Fallout 2 (1998)', icon: '/img/icons/fallout2.png' },
+		{ id: 'Tactics', name: 'Fallout Tactics (2001)', icon: '/img/icons/tactics.png' },
 		// { id: 'Brotherhood of Steel', name: 'Fallout: Brotherhood of Steel (2004)', icon: '/img/icons/fallout-bos.png', disabled: true },
 		{ id: 'Fallout 3', name: 'Fallout 3 (2008)', icon: '/img/icons/fallout3.png' },
 		{ id: 'New Vegas', name: 'Fallout: New Vegas (2010)', icon: '/img/icons/newvegas.png' },
