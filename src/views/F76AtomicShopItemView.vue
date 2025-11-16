96ec953ae713451fbf0590b3e7a18708a6db45e1
Alex Kiselyov <akiselyov@ukr.net>
2025-11-16 17:07:30 +0400

    test

--- src/views/F76AtomicShopItemView.vue
+++ src/views/F76AtomicShopItemView.vue
@@ -329,11 +329,6 @@ onBeforeRouteLeave(() => {
 						<template v-if="item.nameRu">
 							<div class="p-3 card-wrapper book-info-card-sticky">
 								<div class="card">
-									<div v-if="!isMobile" class="card-element book-icon">
-										<a :href="prepareAtomicShopImage(item.mainImage)">
-											<img :src="prepareAtomicShopImage(item.mainImage)" :alt="item.nameRu || item.nameEn || 'Atomic Shop Item'" class="main-image">
-										</a>
-									</div>
 									<div v-if="categoryInfo" class="card-element">
 										<div class="card-subtitle">Категория</div>
 										<RouterLink :to="`/f76-atomic-shop/category/${categoryInfo.formId}-${categoryInfo.slug}`" class="text-decoration-none" @mouseenter="prefetchCategory(categoryInfo.formId)">
