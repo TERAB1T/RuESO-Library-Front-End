05196f67aa2dd05a1dc5d9f878c1580ed4638c41
Alex Kiselyov <akiselyov@ukr.net>
2025-11-16 17:45:12 +0400

    test

--- src/views/F76AtomicShopItemView.vue
+++ src/views/F76AtomicShopItemView.vue
@@ -8,11 +8,16 @@ import Breadcrumb from '@/components/Breadcrumb.vue';
 import NotFoundView from '@/views/NotFoundView.vue';
 import { useQueryClient } from '@tanstack/vue-query';
 import { useWindowSize } from '@vueuse/core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import PhotoSwipeLightbox from 'photoswipe/lightbox';
+import 'photoswipe/style.css';
 
 import type { AtomicShopItem, AtomicShopCategoryWithSubcategories, BreadcrumbItem } from '@/types';
 
 const route = useRoute();
 
+let lightbox: PhotoSwipeLightbox | null = null;
 
 const currentItemFormId = computed(() => (route.params.itemFormId || '') as string);
 
@@ -74,6 +79,68 @@ const updateMetaTags = (itemData: AtomicShopItem) => {
 	});
 }
 
+const initLightbox = () => {
+	if (import.meta.env.SSR) return;
+
+	if (lightbox) {
+		lightbox.destroy();
+		lightbox = null;
+	}
+
+	nextTick(() => {
+		const galleryElement = document.querySelector('#main-item-container');
+		if (!galleryElement) return;
+
+		const sizeCache = new Map();
+
+		lightbox = new PhotoSwipeLightbox({
+			gallery: '#main-item-container',
+			children: '.screenshot-link',
+			pswpModule: () => import('photoswipe'),
+		});
+
+		lightbox.addFilter('itemData', (itemData, index) => {
+			const imgSrc = itemData.src || itemData.element?.href || '';
+
+			if (sizeCache.has(imgSrc)) {
+				const cached = sizeCache.get(imgSrc);
+				itemData.width = cached.width;
+				itemData.height = cached.height;
+				return itemData;
+			}
+
+
+			const existingImg = itemData.element?.querySelector('img');
+			if (existingImg?.complete && existingImg.naturalWidth && existingImg.naturalHeight) {
+				itemData.width = existingImg.naturalWidth;
+				itemData.height = existingImg.naturalHeight;
+				sizeCache.set(imgSrc, { width: existingImg.naturalWidth, height: existingImg.naturalHeight });
+			} else {
+
+				itemData.width = 512;
+				itemData.height = 512;
+
+				const img = new Image();
+				img.onload = () => {
+					sizeCache.set(imgSrc, {
+						width: img.naturalWidth,
+						height: img.naturalHeight
+					});
+
+					if (lightbox?.pswp && lightbox.pswp.currSlide?.index === index) {
+						lightbox.pswp.updateSize(true);
+					}
+				};
+				img.src = imgSrc;
+			}
+
+			return itemData;
+		});
+
+		lightbox.init();
+	});
+};
+
 watchEffect(() => {
 	if (item.value.nameRu) {
 		updateMetaTags(item.value);
@@ -93,10 +160,9 @@ const prefetchCategory = (categoryFormId: string | undefined) => usePrefetchAtom
 const prefetchSubcategory = (subcategoryFormId: string | undefined) => usePrefetchAtomicShopSubcategory(queryClient, subcategoryFormId);
 
 const { width } = useWindowSize();
-const isMobile = computed(() => {
-	if (typeof window === 'undefined') return false;
-	return width.value <= 991;
-});
+const isMobile = computed(() => width.value <= 991);
+const initialDisabled = ref(!isMobile.value);
+
 const infoTabTrigger = ref<HTMLElement | null>(null);
 
 onMounted(async () => {
@@ -114,6 +180,12 @@ onMounted(async () => {
 			}
 		}
 	});
+
+	nextTick(() => {
+		initialDisabled.value = !isMobile.value;
+	});
+
+	initLightbox();
 });
 
 const splittedScreenshots = computed(() => {
@@ -173,6 +245,17 @@ const breadcrumbItems = computed<BreadcrumbItem[]>(() => {
 	});
 
 	return items;
+});
+
+watch(() => item.value.mainImage, () => {
+	initLightbox();
+});
+
+onBeforeUnmount(() => {
+	if (lightbox) {
+		lightbox.destroy();
+		lightbox = null;
+	}
 });
 
 const showTeleport = ref(true);
@@ -248,7 +331,7 @@ onBeforeRouteLeave(() => {
 				</div>
 
 				<div class="col-lg-4">
-					<Teleport v-if="showTeleport" defer to="#info-pane" :disabled="!isMobile">
+					<Teleport v-if="showTeleport" defer to="#info-pane" :disabled="initialDisabled">
 						<template v-if="item.nameRu">
 							<div class="p-3 card-wrapper book-info-card-sticky">
 								<div class="card">
